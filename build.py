54c6d637be16470742c5a4780a6b81970dea55f9
Arthur White <arthur@whitedevops.com>
2018-03-19 00:13:43 +0100

    Replace TypeScript with Python

---
+++ build.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+"""Generate White and White Night themes."""
+
+import copy
+import definitions
+import json
+
+
+class Color(object):
+    """Color defines a color."""
+
+    def __init__(self, hex6, alpha=1):
+        """Make a Color.
+
+        :param hex6: the 6 digits hexadecimal representation
+        :param alpha: the alpha value between 0 and 1
+        """
+        if hex6.startswith('#'):
+            hex6 = hex6[1:]
+        if len(hex6) != 6:
+            raise ValueError('Hexadecimal value must have 6 chars')
+        hex6 = hex6.lower()
+
+        self._rgb = int(hex6, 16)
+        self.alpha = alpha
+
+    @property
+    def alpha(self):
+        """Get the alpha of the color, always between 0 and 1."""
+        return self._alpha
+
+    @alpha.setter
+    def alpha(self, v):
+        if not 0 <= v <= 1:
+            raise ValueError('Alpha must be between 0 and 1')
+        self._alpha = v
+
+    def __str__(self):
+        """Get the shortest hexadecimal color value."""
+        s = '{:06x}'.format(self._rgb)
+        if self.alpha < 1:
+            s += '{:02x}'.format(int(self.alpha*255))
+        if s[0] == s[1] and s[2] == s[3] and s[4] == s[5]:
+            if len(s) == 6:
+                s = s[0] + s[2] + s[4]
+            elif s[6] == s[7]:
+                s = s[0] + s[2] + s[4] + s[6]
+        return '#'+s
+
+
+class ColorSplit(object):
+    """Contains colors for White and White Night themes."""
+
+    def __init__(self, white, white_night=None):
+        """Make a color split for White and White Night themes."""
+        if isinstance(white, ColorSplit):
+            self.white = copy.deepcopy(white.white)
+        else:
+            self.white = Color(white)
+
+        if white_night:
+            if isinstance(white_night, ColorSplit):
+                self.white_night = copy.deepcopy(white_night.white_night)
+            else:
+                self.white_night = Color(white_night)
+        else:
+            self.white_night = copy.deepcopy(self.white)
+
+    def set_alpha(self, white, white_night=None):
+        """Set alpha values of colors.
+
+        :rtype: ColorSplit
+        """
+        self.white.alpha = white
+        self.white_night.alpha = white_night if white_night else white
+        return self
+
+    def with_alpha(self, white, white_night=None):
+        """Set alpha values of colors on a color split copy.
+
+        :rtype: ColorSplit
+        """
+        color_split = copy.deepcopy(self)
+        color_split.set_alpha(white, white_night)
+        return color_split
+
+
+def forge(dd, night=False):
+    """Replace color splits with plain strings."""
+    for d in dd:
+        if isinstance(dd[d], dict):
+            forge(dd[d], night)
+        elif isinstance(dd[d], list):
+            for i, v in enumerate(dd[d]):
+                if isinstance(v, dict):
+                    forge(dd[d][i], night)
+        elif isinstance(dd[d], definitions.ColorSplit):
+            dd[d] = str(dd[d].white) if not night else str(
+                dd[d].white_night)
+
+
+def dump(night=False):
+    """Write definitions to JSON files."""
+    filepath = 'themes/White'
+    if night:
+        filepath += '-Night'
+    filepath += '-color-theme.json'
+
+    with open(filepath, 'w') as f:
+        dd = copy.deepcopy(definitions.definitions)
+        forge(dd, night)
+        json.dump(dd, f, indent=4)
+
+
+if __name__ == '__main__':
+    dump()
+    dump(True)
---
+++ definitions.py
@@ -0,0 +1,262 @@
+"""Themes color definitions."""
+
+from build import ColorSplit
+
+
+colorNone = ColorSplit('#000000').set_alpha(0)
+colorWhite = ColorSplit('#ffffff')
+colorBlack = ColorSplit('#000000')
+colorGrey = ColorSplit('#bec9d3', '#2e2e37')
+colorGreyStrong = ColorSplit('#6b7a88', '#a8a8b1')
+colorGreyLight = ColorSplit('#e7ecf2', '#1e1d27')
+colorGreyOnBlack = ColorSplit('#65696e', colorGrey)
+
+colorPurple = ColorSplit('#7d46fc')
+colorBlue = ColorSplit('#004bff')
+colorCyan = ColorSplit('#00d2ff')
+colorGreen = ColorSplit('#00ff68')
+colorYellow = ColorSplit('#ffca00')
+colorOrange = ColorSplit('#ff9900')
+colorRed = ColorSplit('#ff0032')
+
+colorMain = ColorSplit(colorWhite, colorBlack)
+colorMainNegative = ColorSplit(colorBlack, colorWhite)
+colorMainContrast = ColorSplit('#f4f6fc', '#0a0b0f')
+colorMainContrastTransparent = ColorSplit('#99a7e1', '#616a93').set_alpha(0.1)
+
+colorPrimary = ColorSplit(colorBlue, colorPurple)
+colorPrimaryNegative = colorWhite
+colorPrimaryHighlight = ColorSplit(
+    '#0064ff', colorPrimary).set_alpha(0.15, 0.25)
+colorPrimaryHighlightStrong = colorPrimaryHighlight.with_alpha(0.3, 0.5)
+colorPrimaryHighlightLight = colorPrimaryHighlight.with_alpha(0.05, 0.08)
+
+colorAdded = colorGreen
+colorAddedHighlight = colorAdded.with_alpha(0.1, 0.2)
+colorDeleted = colorRed
+colorDeletedHighlight = colorDeleted.with_alpha(0.1, 0.2)
+
+colorError = colorRed
+colorWarning = colorOrange
+
+colorFind = colorYellow.with_alpha(0.6)
+colorFindHighlight = colorFind.with_alpha(0.3)
+
+colorBorder = colorGreyLight
+
+# Theme settings
+
+definitions = {
+    'colors': {
+        'activityBar.background': colorMain,
+        'activityBar.border': colorNone,
+        'activityBar.foreground': colorGrey,
+        'activityBarBadge.background': colorMainNegative,
+        'activityBarBadge.foreground': colorMain,
+        'badge.background': colorMainNegative,
+        'badge.foreground': colorMain,
+        'button.background': colorPrimary,
+        'button.foreground': colorPrimaryNegative,
+        'contrastBorder': colorNone,
+        'diffEditor.insertedTextBackground': colorAddedHighlight,
+        'diffEditor.removedTextBackground': colorDeletedHighlight,
+        'dropdown.background': colorMainContrast,
+        'dropdown.border': colorBorder,
+        'dropdown.foreground': colorMainNegative,
+        'editor.background': colorMain,
+        'editor.findMatchBackground': colorFind,
+        'editor.findMatchHighlightBackground': colorFindHighlight,
+        'editor.foreground': colorMainNegative,
+        'editor.lineHighlightBackground': colorPrimaryHighlightLight,
+        'editor.lineHighlightBorder': colorNone,
+        'editor.selectionBackground': colorPrimaryHighlightStrong,
+        'editor.selectionForeground': colorMain,
+        'editor.wordHighlightBackground': colorPrimaryHighlight,
+        'editor.wordHighlightStrongBackground': colorPrimaryHighlightStrong,
+        'editorBracketMatch.background': colorPrimaryHighlightStrong,
+        'editorBracketMatch.border': colorNone,
+        'editorCursor.foreground': colorPrimary,
+        'editorError.foreground': colorError,
+        'editorGroup.border': colorMainContrast,
+        'editorGroupHeader.tabsBackground': colorMain,
+        'editorGroupHeader.tabsBorder': colorNone,
+        'editorGutter.addedBackground': colorAdded,
+        'editorGutter.deletedBackground': colorDeleted,
+        'editorGutter.modifiedBackground': colorMainNegative,
+        'editorIndentGuide.background': colorMainContrast,
+        'editorLineNumber.foreground': ColorSplit('#ebf0f5', colorGreyLight),
+        'editorLink.activeForeground': colorPrimary,
+        'editorOverviewRuler.addedForeground': colorNone,
+        'editorOverviewRuler.border': colorNone,
+        'editorOverviewRuler.bracketMatchForeground':
+            colorPrimaryHighlightStrong,
+        'editorOverviewRuler.errorForeground': colorError,
+        'editorOverviewRuler.findMatchForeground': colorFind,
+        'editorOverviewRuler.modifiedForeground': colorMainNegative,
+        'editorOverviewRuler.selectionHighlightForeground':
+            colorPrimaryHighlightStrong,
+        'editorOverviewRuler.warningForeground': colorWarning,
+        'editorOverviewRuler.wordHighlightForeground': colorPrimaryHighlight,
+        'editorOverviewRuler.wordHighlightStrongForeground':
+            colorPrimaryHighlightStrong,
+        'editorRuler.foreground': colorMainContrast,
+        'editorSuggestWidget.foreground': colorMainNegative,
+        'editorWarning.foreground': colorWarning,
+        'editorWidget.background': colorMainContrast,
+        'editorWidget.border': colorBorder,
+        'errorForeground': colorError,
+        'extensionButton.prominentBackground': colorPrimary,
+        'extensionButton.prominentForeground': colorPrimaryNegative,
+        'extensionButton.prominentHoverBackground': colorPrimary,
+        'focusBorder': colorBorder,
+        'foreground': colorMainNegative,
+        'gitDecoration.ignoredResourceForeground': colorGrey,
+        'gitDecoration.modifiedResourceForeground': colorMainNegative,
+        'gitDecoration.untrackedResourceForeground': colorMainNegative,
+        'input.background': colorMainContrast,
+        'input.border': colorBorder,
+        'input.foreground': colorMainNegative,
+        'input.placeholderForeground': colorGrey,
+        'list.activeSelectionBackground': colorMainNegative,
+        'list.activeSelectionForeground': colorMain,
+        'list.dropBackground': colorPrimaryHighlightStrong,
+        'list.focusBackground': colorPrimaryHighlight,
+        'list.focusForeground': colorMainNegative,
+        'list.highlightForeground': colorMainNegative,
+        'list.hoverBackground': colorMainContrast,
+        'list.inactiveSelectionBackground': colorMainContrast,
+        'list.inactiveSelectionForeground': colorMainNegative,
+        'panel.background': colorMainContrast,
+        'panel.border': colorNone,
+        'panelTitle.activeBorder': colorGrey,
+        'peekView.border': colorMainNegative,
+        'peekViewEditor.background': colorNone,
+        'peekViewEditor.matchHighlightBackground': colorFind,
+        'peekViewResult.background': colorNone,
+        'peekViewResult.fileForeground': colorGreyStrong,
+        'peekViewResult.lineForeground': colorGreyStrong,
+        'peekViewResult.matchHighlightBackground': colorFind,
+        'peekViewResult.selectionBackground': colorMainNegative,
+        'peekViewResult.selectionForeground': colorMain,
+        'peekViewTitle.background': colorNone,
+        'peekViewTitleDescription.foreground': colorGrey,
+        'peekViewTitleLabel.foreground': colorMainNegative,
+        'progressBar.background': colorPrimary,
+        'scrollbar.shadow': colorNone,
+        'scrollbarSlider.background': colorMainContrastTransparent,
+        'scrollbarSlider.hoverBackground': colorMainNegative,
+        'sideBar.background': colorMain,
+        'sideBar.foreground': colorGreyStrong,
+        'sideBarSectionHeader.background': colorMainContrast,
+        'sideBarSectionHeader.foreground': colorGreyStrong,
+        'sideBarTitle.foreground': colorGrey,
+        'statusBar.background': colorBlack,
+        'statusBar.foreground': colorGreyOnBlack,
+        'statusBar.noFolderBackground': colorBlack,
+        'tab.activeBackground': colorMainNegative,
+        'tab.activeForeground': colorMain,
+        'tab.border': colorNone,
+        'tab.inactiveBackground': colorNone,
+        'tab.inactiveForeground': colorGrey,
+        'terminal.ansiBlack': colorMainNegative,
+        'terminal.ansiBlue': colorBlue,
+        'terminal.ansiBrightBlack': colorMainNegative,
+        'terminal.ansiBrightBlue': colorBlue,
+        'terminal.ansiBrightCyan': colorCyan,
+        'terminal.ansiBrightGreen': colorGreen,
+        'terminal.ansiBrightMagenta': colorPurple,
+        'terminal.ansiBrightRed': colorRed,
+        'terminal.ansiBrightWhite': colorGreyStrong,
+        'terminal.ansiBrightYellow': colorYellow,
+        'terminal.ansiCyan': colorCyan,
+        'terminal.ansiGreen': colorGreen,
+        'terminal.ansiMagenta': colorPurple,
+        'terminal.ansiRed': colorRed,
+        'terminal.ansiWhite': colorGreyStrong,
+        'terminal.ansiYellow': colorYellow,
+        'terminal.foreground': colorMainNegative,
+        'terminal.selectionBackground': colorPrimaryHighlightStrong,
+        'terminalCursor.foreground': colorPrimary,
+        'textLink.activeForeground': colorPrimary,
+        'textLink.foreground': colorPrimary,
+        'titleBar.activeBackground': colorMain,
+        'titleBar.activeForeground': colorGreyStrong,
+        'titleBar.inactiveBackground': colorMain,
+        'titleBar.inactiveForeground': colorGrey,
+        'welcomePage.buttonBackground': colorMainContrast,
+        'welcomePage.buttonHoverBackground': colorPrimaryHighlight,
+        'widget.shadow': colorNone,
+    },
+    'tokenColors': [
+        {
+            'scope': [
+                'comment',
+                'string.quoted.docstring'
+            ],
+            'settings': {
+                'foreground': colorGrey,
+            },
+        },
+        {
+            'scope': [
+                'string'
+            ],
+            'settings': {
+                'foreground': colorGreyStrong,
+            },
+        },
+        {
+            'scope': [
+                'punctuation.definition.string',
+                'storage.type.string.python',
+            ],
+            'settings': {
+                'foreground': colorMainNegative,
+            },
+        },
+        {
+            'scope': [
+                'beginning.punctuation',
+                'entity.name.section.group-title',
+                'entity.name.tag',
+                'entity.other.attribute-name.class',
+                'entity.other.attribute-name.id',
+                'keyword.const',
+                'keyword.control',
+                'keyword.function',
+                'keyword.import',
+                'keyword.operator.assignment',
+                'keyword.operator.comparison',
+                'keyword.operator.decrement',
+                'keyword.operator.expression',
+                'keyword.operator.increment',
+                'keyword.operator.increment-decrement',
+                'keyword.operator.logical',
+                'keyword.operator.new',
+                'keyword.operator.other',
+                'keyword.other.special-method',
+                'keyword.package',
+                'keyword.type',
+                'keyword.var',
+                'markup.heading',
+                'meta.function.decorator',
+                'meta.tag.sgml.doctype.html',
+                'punctuation.separator.key-value',
+                'storage.modifier',
+                'storage.type.class',
+                'storage.type.enum',
+                'storage.type.function',
+                'storage.type.import',
+                'storage.type.interface',
+                'storage.type.js',
+                'storage.type.namespace',
+                'storage.type.property',
+                'storage.type.string.python',
+                'storage.type.ts',
+            ],
+            'settings': {
+                'fontStyle': 'bold',
+            },
+        },
+    ],
+}
